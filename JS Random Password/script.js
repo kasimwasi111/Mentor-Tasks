f4c107291b03e4df30a19b7721bc719de6a89ef9
Mohaamad Kasim <kasimwasi111@gmail.com>
2024-01-08 23:27:50 +0530

    done

--- JS Random Password/script.js
+++ JS Random Password/script.js
@@ -2,7 +2,7 @@ const passwordBox = document.getElementById("password");
 const generateBtn = document.getElementById("btn");
 const copyPass = document.getElementById("copy");
 
-const passwordLength = 8;
+const passwordLength = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -13,11 +13,6 @@ const allChars = upperCase + lowerCase + numbers + symbols;
 
 function createPassword() {
   let password = "";
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += numbers[Math.floor(Math.random() * numbers.length)];
-  password += symbols[Math.floor(Math.random() * symbols.length)];
-
   while (passwordLength > password.length) {
     password += allChars[Math.floor(Math.random() * allChars.length)];
   }
